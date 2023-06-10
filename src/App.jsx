2aebd0eeef97c8c56156bd58c7a26a743981b383
LMuzzupappa <leandro.muzzupappa@mediamonks.com>
2023-06-10 03:28:22 -0300

    Create ItemListContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Navbar } from "./components/Navbar/Navbar";
 import "./App.css";
+import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
 
 function App() {
   return (
     <>
       <Navbar />
+      <ItemListContainer greeting="Oreos" />
     </>
   );
 }
---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable react/prop-types */
+export const ItemListContainer = ({ greeting }) => {
+  return (
+    <div>
+      <h1>{greeting}</h1>
+    </div>
+  );
+};
