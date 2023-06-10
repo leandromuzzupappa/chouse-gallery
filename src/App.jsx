cf1e50172aec91ffa899da0f182afc8589d2b037
LMuzzupappa <leandro.muzzupappa@mediamonks.com>
2023-06-10 03:29:33 -0300

    PreEntrega1 Muzzupappa Leandro

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Navbar } from "./components/Navbar/Navbar";
 import "./App.css";
 import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
 
+/* Pre entrega 1 - Muzzupappa */
+
 function App() {
   return (
     <>
