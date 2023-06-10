d30b4f534f8a32cfda497f2c1b13b6c9b7f4a40f
LMuzzupappa <leandro.muzzupappa@mediamonks.com>
2023-06-10 03:21:57 -0300

    Create NavbarMenu component

--- src/components/NavbarMenu/NavbarMenu.jsx
+++ src/components/NavbarMenu/NavbarMenu.jsx
@@ -1,9 +1,14 @@
+/* eslint-disable react/prop-types */
 import "./NavbarMenu.css";
 
-export const NavbarMenu = () => {
+export const NavbarMenu = ({ isOpen }) => {
   return (
-    <div className="navbarMenu">
-      <h1>oreos</h1>
+    <div className="navbarMenu" data-open={isOpen}>
+      <ul>
+        <li>Glass and Iron</li>
+        <li>Art Pottery</li>
+        <li>Art Deco</li>
+      </ul>
     </div>
   );
 };
