ada6a7349faa7dbd6116aae9e4dbb86a6dcebd0a
LMuzzupappa <leandro.muzzupappa@mediamonks.com>
2023-06-10 02:58:29 -0300

    Update Navbar with animations and add navbar actions with login button and cart widget

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,41 @@
+import { useState } from "react";
+import { CartWidget } from "../CartWidget/CartWidget";
+
 import "./Navbar.css";
 
 export const Navbar = () => {
-  return (
-    <nav className="navbar">
-      <div className="navbar--logo">
-        <h1>Pepitos</h1>
-      </div>
+  const [navbarOpen, setNavbarOpen] = useState(false);
 
-      <button>Menu</button>
-    </nav>
+  const onMenuClick = () => {
+    setNavbarOpen(!navbarOpen);
+  };
+
+  return (
+    <>
+      <nav className={`navbar ${navbarOpen ? "navbar-active" : ""}`}>
+        <button
+          className={`navbar-menu--btn ${
+            navbarOpen ? "navbar-menu--btn-active" : ""
+          }`}
+          onClick={onMenuClick}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+
+        <div className="navbar--logo">
+          <h2>Pepitos</h2>
+        </div>
+
+        <div className="navbar--actions">
+          <CartWidget />
+          <button className="login">Login</button>
+        </div>
+      </nav>
+
+      {navbarOpen && <h1>mostrar menu</h1>}
+    </>
   );
 };
