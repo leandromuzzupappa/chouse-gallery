63df589c976767f3bc37972eca1a7afaa5a436b6
LMuzzupappa <leandro.muzzupappa@mediamonks.com>
2023-06-17 02:25:57 -0300

    Create data.js with dummy data

---
+++ src/data/data.js
@@ -0,0 +1,27 @@
+export const galleries = [
+  {
+    id: 1,
+    name: "Paintings",
+    link: "#link1",
+  },
+  {
+    id: 2,
+    name: "Photography",
+    link: "#link2",
+  },
+  {
+    id: 3,
+    name: "Illustration",
+    link: "#link3",
+  },
+  {
+    id: 4,
+    name: "Street Art",
+    link: "#link4",
+  },
+  {
+    id: 5,
+    name: "Digital Art",
+    link: "#link5",
+  },
+];
