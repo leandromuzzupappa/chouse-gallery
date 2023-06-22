629f2f32d26e8f9f175bee5578010a0ea16181a3
LMuzzupappa <leandro.muzzupappa@mediamonks.com>
2023-06-22 09:35:24 -0300

    Implement ArtGallery and ArtGalleryLow components in the Experience

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,17 +1,34 @@
 /* eslint-disable react/no-unknown-property */
-import { OrbitControls } from "@react-three/drei";
+import { Suspense } from "react";
+import {
+  OrbitControls,
+  TransformControls,
+  PerspectiveCamera,
+} from "@react-three/drei";
+import { ArtGallery } from "../models/ArtGallery/ArtGallery";
+import { ArtGalleryLow } from "../models/ArtGalleryLow/ArtGalleryLow";
 
 export const Experience = () => {
   return (
     <>
-      <OrbitControls makeDefault />
+      <OrbitControls makeDefault enableZoom={true} />
 
-      <pointLight position={[10, 10, 10]} intensity={0.9} />
+      <TransformControls mode="translate" position={[0, 0, 0]}>
+        <pointLight position={[0, 5, 0]} intensity={0.6} color="#fff5e2" />
+      </TransformControls>
+
+      <PerspectiveCamera position={[0, 1, 5]} fov={75} makeDefault />
 
       <mesh>
         <boxGeometry />
         <meshStandardMaterial color="green" />
       </mesh>
+
+      {/* <ArtGalleryLow /> */}
+
+      <Suspense fallback={<ArtGalleryLow />}>
+        <ArtGallery />
+      </Suspense>
     </>
   );
 };
