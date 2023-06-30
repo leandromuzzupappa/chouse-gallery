c9b6de0f1c1460613d94195036186fce36f01514
LMuzzupappa <leandro.muzzupappa@mediamonks.com>
2023-06-30 02:40:40 -0300

    Add postprocessing effect

--- src/pages/Gallery/GalleryPage.jsx
+++ src/pages/Gallery/GalleryPage.jsx
@@ -1,5 +1,6 @@
 import { Canvas } from "@react-three/fiber";
 import { ScrollControls } from "@react-three/drei";
+import { EffectComposer, Bloom } from "@react-three/postprocessing";
 import { Experience } from "../../components/Experience/Experience";
 import "./GalleryPage.css";
 
@@ -10,6 +11,12 @@ export const GalleryPage = () => {
         <ScrollControls pages={5} damping={0.3}>
           <Experience />
         </ScrollControls>
+
+        {
+          <EffectComposer>
+            <Bloom mipmapBlur luminanceThreshold={0.7} radius={0.5} />
+          </EffectComposer>
+        }
       </Canvas>
     </div>
   );
