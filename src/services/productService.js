b2e4198623b29bc45532a1ec4fe7d18bdf0cf764
LMuzzupappa <leandro.muzzupappa@mediamonks.com>
2023-07-04 02:08:38 -0300

    Create getProductsByCategorySlug method to fetch products

--- src/services/productService.js
+++ src/services/productService.js
@@ -7,3 +7,7 @@ export const getProducts = async () => {
 export const getProductById = async (id) => {
   return productData.find((product) => product.id === id);
 };
+
+export const getProductsByCategorySlug = async (slug) => {
+  return productData.filter((product) => product.category === slug);
+};
