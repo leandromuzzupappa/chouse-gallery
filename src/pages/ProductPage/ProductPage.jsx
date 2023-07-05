fa660738652357a4c7318b93b9bc41171e7e2a91
LMuzzupappa <leandro.muzzupappa@mediamonks.com>
2023-07-04 23:50:28 -0300

    PreEntrega 2 - Muzzupappa Leandro

--- src/pages/ProductPage/ProductPage.jsx
+++ src/pages/ProductPage/ProductPage.jsx
@@ -1,26 +1,69 @@
+import "./ProductPage.css";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams, Link } from "react-router-dom";
 import { getProductById } from "../../services/productService";
+
+const pattern = Math.random() > 0.5 ? "bg-rectangles" : "bg-rhombus";
 
 export const ProductPage = () => {
   const { productId } = useParams();
+  const navigate = useNavigate();
   const [product, setProduct] = useState({});
+  const [quantity, setQuantity] = useState(1);
 
   useEffect(() => {
     const getProductData = async () => {
       const product = await getProductById(productId);
 
+      console.log(product);
       setProduct(product);
     };
 
     getProductData();
   }, [productId]);
 
+  const increaseQuantity = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const decreaseQuantity = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  };
+
   return (
-    <div className="product">
-      <h1>{product.name}</h1>
-      <p>{product.category}</p>
-      <img src={product.image} alt={product.name} />
-    </div>
+    <section className={`product-page product-page--${pattern}`}>
+      <div className="product-page--content">
+        <Link
+          to={".."}
+          onClick={(e) => {
+            e.preventDefault();
+            navigate(-1);
+          }}
+        >
+          Back to {product.category}
+        </Link>
+        <h1>{product.name}</h1>
+        <p>{product.dimentions}</p>
+
+        <div className="product-page--content-actions">
+          <div className="price">
+            <span>{product.price}</span>
+
+            <div className="quantity">
+              <button onClick={decreaseQuantity}>-</button>
+              <span>{quantity}</span>
+              <button onClick={increaseQuantity}>+</button>
+            </div>
+          </div>
+
+          <button className="addToCart">Add to cart</button>
+        </div>
+      </div>
+      <div className="product-page--media">
+        <img src={product.image} alt={product.name} />
+      </div>
+    </section>
   );
 };
