d3bdf7710110d8767232e6c0d1648afd098280e5
LMuzzupappa <leandro.muzzupappa@mediamonks.com>
2023-07-27 04:01:05 -0300

    feat(vite.config): update config to handle glsl

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { glslify } from "vite-plugin-glslify";
+import glslifyRollup from "rollup-plugin-glslify";
+
+const glslifyOptions = {
+  extensions: ["vs", "fs", ".glsl", ".frag.shader"],
+  compress: true,
+  transform: ["glslify-import"],
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), glslify(glslifyOptions), glslifyRollup(glslifyOptions)],
 });
