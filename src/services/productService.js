20e6d7eb155464919dfffa2f3e29c26d9f282f39
LMuzzupappa <leandro.muzzupappa@mediamonks.com>
2023-07-27 02:21:48 -0300

    feat(productService): implements product methods that fetches data from firebase

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,13 +1,52 @@
-import productData from "../data/products.json";
+import { db } from "../config/firebase";
+import { collection, getDocs } from "firebase/firestore";
+
+const getProductsSnapshot = async () => {
+  const productsRef = collection(db, "products");
+  const productsSnapshot = await getDocs(productsRef);
+
+  return productsSnapshot;
+};
 
 export const getProducts = async () => {
-  return productData;
+  const snapshot = await getProductsSnapshot();
+  const productsData = snapshot.docs.map((doc) => ({
+    docId: doc.id,
+    ...doc.data(),
+  }));
+
+  return productsData;
 };
 
 export const getProductById = async (id) => {
-  return productData.find((product) => product.id === id);
+  const products = await getProducts();
+  const product = products.find((product) => product.id === id);
+
+  return product;
 };
 
 export const getProductsByCategorySlug = async (slug) => {
-  return productData.filter((product) => product.category === slug);
+  const snapshot = await getProductsSnapshot();
+  const productsData = snapshot.docs.map((doc) => ({
+    docId: doc.id,
+    ...doc.data(),
+  }));
+
+  return productsData.filter((product) => product.category === slug);
 };
+
+export const addProduct = async (product) => {
+  const productsRef = collection(db, "products");
+  await productsRef.add(product);
+};
+
+/* export const addBulkProducts = async () => {
+  try {
+    const productsRef = collection(db, "products");
+    productData.forEach(async (product) => {
+      await addDoc(productsRef, product);
+    });
+  } catch (error) {
+    console.error("Error adding product: ", error);
+  }
+}; */
