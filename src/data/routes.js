eced792ac3c48b83a7d41c18e0a2c61d2ba0cc45
LMuzzupappa <leandro.muzzupappa@mediamonks.com>
2023-07-28 17:50:34 -0300

    feat(routes): add checkout route

--- src/data/routes.js
+++ src/data/routes.js
@@ -1,6 +1,7 @@
 import { HomePage } from "../pages/HomePage/HomePage";
 import { GalleryGrid } from "../pages/GalleryGrid/GalleryGrid";
 import { ProductPage } from "../pages/ProductPage/ProductPage";
+import { CheckoutPage } from "../pages/CheckoutPage/CheckoutPage";
 
 export const routes = [
   {
@@ -17,5 +18,10 @@ export const routes = [
     id: 2,
     path: "/:productId/:productName",
     element: ProductPage,
+  },
+  {
+    id: 3,
+    path: "/checkout",
+    element: CheckoutPage,
   },
 ];
