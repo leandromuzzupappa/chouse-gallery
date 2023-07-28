483db55287d0feae15dd8374c84a4165ea7e838a
LMuzzupappa <leandro.muzzupappa@mediamonks.com>
2023-07-28 14:44:51 -0300

    refactor(CartWidget): update component to consume the context

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,9 +1,13 @@
 import "./CartWidget.css";
+import { useContext } from "react";
+import { CartContext } from "../../context/CartContext";
 
 export const CartWidget = () => {
+  const { getQuantity } = useContext(CartContext);
+
   return (
     <button className="cart">
-      [ <span className="cart--count">12</span> ]
+      [ <span className="cart--count">{getQuantity()}</span> ]
     </button>
   );
 };
