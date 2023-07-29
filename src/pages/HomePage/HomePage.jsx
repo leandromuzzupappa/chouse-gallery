fd0376a32cb282e22ed9938e283edc206cc2946d
LMuzzupappa <leandro.muzzupappa@mediamonks.com>
2023-07-28 22:13:31 -0300

    feat(HomePage): add animations

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,16 @@
 import "./Homepage.css";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { gsap } from "gsap";
 import { ProductCard } from "../../components/ProductCard/ProductCard";
 import { getProducts } from "../../services/productService";
+import { LoadingScreen } from "../../components/LoadingScreen/LoadingScreen";
 
 export const HomePage = () => {
   const [products, setProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+
+  const timelineRef = useRef(null);
+  const productListref = useRef(null);
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -23,11 +28,36 @@ export const HomePage = () => {
     return () => {};
   }, []);
 
+  useEffect(() => {
+    const timeline = gsap.timeline({ paused: true });
+
+    if (products) {
+      onProductsLoad(timeline);
+    }
+
+    timelineRef.current = timeline;
+    timelineRef.current.play();
+  }, [products]);
+
+  const onProductsLoad = (timeline) => {
+    timeline.from(
+      productListref.current.children,
+      {
+        duration: 1,
+        opacity: 0,
+        y: -100,
+        stagger: 0.1,
+        ease: "power3.out",
+      },
+      "-=0.5"
+    );
+  };
+
   return (
     <section className="homepage">
-      {isLoading && <p>Loading...</p>}
+      {isLoading && <LoadingScreen />}
 
-      <ul className="homepage--grid">
+      <ul ref={productListref} className="homepage--grid">
         {products.map((product) => (
           <li key={product.docId} className="homepage--grid-item">
             <ProductCard {...product} />
