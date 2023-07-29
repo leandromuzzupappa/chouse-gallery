e03ce644a9a6793c74fffe69cc119167db708976
LMuzzupappa <leandro.muzzupappa@mediamonks.com>
2023-07-28 22:13:55 -0300

    feat(GalleryGrid): add animations

--- src/pages/GalleryGrid/GalleryGrid.jsx
+++ src/pages/GalleryGrid/GalleryGrid.jsx
@@ -1,14 +1,20 @@
 import "./GalleryGrid.css";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { gsap } from "gsap";
 import { useParams } from "react-router-dom";
 import { getCategoryBySlug } from "../../services/categoryService";
 import { getProductsByCategorySlug } from "../../services/productService";
 import { ProductCard } from "../../components/ProductCard/ProductCard";
+import { LoadingScreen } from "../../components/LoadingScreen/LoadingScreen";
 
 export const GalleryGrid = () => {
   const { categoryName } = useParams();
   const [products, setProducts] = useState([]);
   const [category, setCategory] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  const timelineRef = useRef(null);
+  const productListref = useRef(null);
 
   useEffect(() => {
     const getCategoryData = async () => {
@@ -17,16 +23,44 @@ export const GalleryGrid = () => {
 
       setCategory(category);
       setProducts(products);
+      setIsLoading(false);
     };
 
     getCategoryData();
   }, [categoryName]);
 
+  useEffect(() => {
+    const timeline = gsap.timeline({ paused: true });
+
+    if (products) {
+      onProductsLoad(timeline);
+    }
+
+    timelineRef.current = timeline;
+    timelineRef.current.play();
+  }, [products]);
+
+  const onProductsLoad = (timeline) => {
+    timeline.from(
+      productListref.current.children,
+      {
+        duration: 1,
+        opacity: 0,
+        y: -100,
+        stagger: 0.1,
+        ease: "power3.out",
+      },
+      "-=0.5"
+    );
+  };
+
   return (
     <section className="gallery-page">
+      {isLoading && <LoadingScreen />}
+
       <h1 className="gallery-page--title">{category.name}</h1>
 
-      <ul className="gallery-page--grid">
+      <ul ref={productListref} className="gallery-page--grid">
         {products.map((product) => (
           <li key={product.id} className="gallery-page--grid-item">
             <ProductCard {...product} />
